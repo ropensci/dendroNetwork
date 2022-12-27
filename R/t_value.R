3a2087420d3c80053a1981437d6fbb4f2c7f4cd1
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2022-12-27 20:28:28 +0100

    Correction

--- R/t_value.R
+++ R/t_value.R
@@ -3,7 +3,7 @@
 #' Function to determine the Students' t-value using the correlation and the number of compare values (tree-rings)
 #'
 #' @param r correlation value.
-#' @param n number over overlapping tree-rings/compared values.
+#' @param n number of overlapping tree-rings/compared values.
 #' @returns Students' t value as a numeric.
 #' @examples
 #' t_value(0.5, 100)
