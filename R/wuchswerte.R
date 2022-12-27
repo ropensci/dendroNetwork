d6cd33c573369a75e834b03ded4f6abdba62487b
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2022-12-27 20:35:15 +0100

    wuchswerte

--- R/wuchswerte.R
+++ R/wuchswerte.R
@@ -1,8 +1,15 @@
-# function to normalize according to Hollsteins transformation to Wuchswerte
-# wuchswerte Hollstein(1980, 14-15) Y(i)=100 ln (b(i)/b(i-1))
-# eLog(w(n)/w(n+1)) 
-# author: Ronald M. Visser
-# to convert rwl to wuchwerte use: as.rwl(apply(treering_rwl, 2, wuchswerte))
+#' Wuchswerte (Hollstein 1980)
+#'
+#' Function to normalize tree-ring values according to Hollsteins transformation to Wuchswerte
+#'
+#' Published on pages 14-15 in Hollstein, E. 1980. Mitteleuropäische Eichenchronologie. Trierer Dendrochronologische Forschungen zur Archäologie und Kunstgeschichte. Trierer Grabungen und Forschungen 11. Mainz am Rhein: Verlag Philipp von Zabern.
+#'
+#' @param x tree-ring series
+#' @returns tree-ring series normalized according to Hollstein (1980, 14-15)
+#' @examples
+#' wuchswerte(trs)
+#' to convert rwl to wuchwerte use: as.rwl(apply(treering_rwl, 2, wuchswerte))
+
 wuchswerte <- function(x) {
   x2 <- c(NA,x[-length(x)])
   wuchswerte <- 100*log(x/x2)
