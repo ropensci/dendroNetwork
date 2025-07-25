2e0775f4162ede6358079fb177441306ede86ee0
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2025-07-25 12:46:24 +0200

    making r_hol a global variable

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 # defining global variables
-utils::globalVariables(c("GN_com", "com", "com_name", "i", "n", "node", "p", "r", "series_a", "series_b", "sgc"))
+utils::globalVariables(c("GN_com", "com", "com_name", "i", "n", "node", "p", "r", "r_hol", "series_a", "series_b", "sgc"))
